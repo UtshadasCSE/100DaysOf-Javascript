03d2e87acb2d7794d8c196b81b2be60b92b920a7
Utsha Kumar Das <75168319+UtshadasCSE@users.noreply.github.com>
2023-08-04 00:05:43 +0600

    Add files via upload

---
+++ Day2.js
@@ -0,0 +1,10 @@
+let name = "utsha";
+let age = 22;
+console.log(name);
+if (age == 22) {
+  console.log("Your age is 22");
+} else {
+  console.log("I don't know your name ");
+}
+let lastName = "Das";
+console.log(name + lastName);
