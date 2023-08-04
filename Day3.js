e71188c37876749daa9f1ca042f2ecbf7c812cdf
Utsha Kumar Das <75168319+UtshadasCSE@users.noreply.github.com>
2023-08-04 15:57:48 +0600

    Add files via upload

---
+++ Day3.js
@@ -0,0 +1,20 @@
+let country = "Bangladesh";
+let len = country.length;
+console.log(len);
+
+let rep = country.replace("B", "D");
+console.log(rep);
+
+let info = "Javascript is not same as java";
+let inDex = info.indexOf("java");
+console.log(inDex);
+
+let jav = "Javascript is most demandable language";
+let res = jav.startsWith("Javascript");
+let sp = jav.split(1, 2);
+console.log(res);
+console.log(jav.toLocaleUpperCase());
+console.log(jav.toLocaleLowerCase());
+console.log(sp);
+let lenN = 'hello"';
+console.log(lenN.length);
