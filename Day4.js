4b7ed5575b869a512a2e445a235d88d595a3e4bc
Utsha Kumar Das <75168319+UtshadasCSE@users.noreply.github.com>
2023-08-04 16:34:25 +0600

    Add files via upload

---
+++ Day4.js
@@ -0,0 +1,16 @@
+let arr = [12, 23, 11, 23, 13, 4324];
+let arr2 = [40, 50, 60, 70, 23, 43, 23];
+let con = arr.toString();
+console.log(typeof arr);
+console.log(typeof con);
+let joinArr = arr.join("-");
+console.log(joinArr);
+console.log(typeof joinArr);
+let poP = arr.pop();
+console.log(arr);
+let pusH = arr.push(201);
+console.log(arr);
+let shFt = arr.shift(12);
+console.log(shFt, arr);
+let conCat = arr.concat(arr2);
+console.log(conCat);
