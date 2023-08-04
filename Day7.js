b2b2c3743e98d58e1b8f0872e3de3f8ac6eef393
Utsha Kumar Das <75168319+UtshadasCSE@users.noreply.github.com>
2023-08-04 23:52:10 +0600

    Add files via upload

---
+++ Day7.js
@@ -0,0 +1,12 @@
+let name = "utsha";
+let lastName = "Das";
+console.log(name == lastName);
+console.log(name > lastName);
+console.log(name < lastName);
+
+let Number = name.length;
+console.log(Number);
+let NumberTwo = lastName.length;
+console.log(Number < NumberTwo);
+
+console.log(name != lastName);
