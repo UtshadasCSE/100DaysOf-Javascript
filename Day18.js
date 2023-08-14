07c0c05bf0e48fb942f127209b4505a6444a06dd
Utsha Kumar Das <75168319+UtshadasCSE@users.noreply.github.com>
2023-08-14 17:35:13 +0600

    Add files via upload

---
+++ Day18.js
@@ -0,0 +1,40 @@
+// [1, 2, 3, 5].map((number) => {
+//   console.log(number);
+// });
+
+// let result = [12, 32, 34, 54].map((number) => {
+//   return number * 2;
+// });
+// console.log(result);
+
+// function sortArray(array) {
+//   array.sort((a, b) => a - b);
+//   return array;
+// }
+// console.log(sortArray([12, 11, 20, 10, 5, 80, 44]));
+
+// function findMax(array) {
+//   let maxNumber = array[0];
+//   for (const number of array) {
+//     if (number > maxNumber) {
+//       maxNumber = number;
+//     }
+//   }
+//   return maxNumber;
+// }
+// console.log(findMax([12, 5, 33, 80, 1120, 1233, 121, 32, 12, 20000]));
+
+// const filter = (numbers, greaterThan) => {
+//   let result = [];
+//   for (const number of numbers) {
+//     if (number > greaterThan) {
+//       result.push(number);
+//     }
+//   }
+//   return result;
+// };
+// console.log(filter([12, 1, 21, 21, 13, 1131, , 1321, 121, 13, 1212], 100));
+
+// let numbers = [10, 12, 16, 70, 39];
+
+// console.log(numbers.filter((num) => num > 10));
